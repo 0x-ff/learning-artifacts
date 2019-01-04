d228b75729319838cd75382031397bd1632db64e
Pavel Ivanisenko <pivanisenko@avito.ru>
2019-01-04 15:09:29 +0300

    es6-cheatsheet: add some cheatsheets.

--- es6-cheatsheet/cheatsheet.js
+++ es6-cheatsheet/cheatsheet.js
@@ -245,3 +245,21 @@ function* generateSequence() {
     yield 2;
     return 3;
 }
+
+//
+// proxy
+let user = {};
+let proxy = new Proxy(user, {
+  get(target, prop) {
+    alert(`Чтение ${prop}`);
+    return target[prop];
+  },
+  set(target, prop, value) {
+    alert(`Запись ${prop} ${value}`);
+    target[prop] = value;
+    return true;
+  }
+});
+proxy.firstName = "Ilya"; // запись
+proxy.firstName; // чтение
+alert(user.firstName); // Ilya
